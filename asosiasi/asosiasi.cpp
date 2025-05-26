9bae8a5610335ea5cac4f9a1dd75f56de28a977b
BagasGandhi <saputramuhammadgandhi@gmail.com>
2025-05-26 11:51:32 +0700

    first commit

---
+++ asosiasi/asosiasi.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// nama reo:
+// RelasidanInheritance_4NIMBelakang
+
+
+class dokter;
+class pasien { //asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
